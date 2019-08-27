dda2b20665fce0642b29bd4a589145027a0a8322
Quentin Flayac <quentin.flayac@epitech.eu>
2019-08-27 11:00:18 +0200

    update, new function

--- SRSRewriter.php
+++ SRSRewriter.php
@@ -1,49 +1,70 @@
 <?php
 
 // --------------------------
-// Flayac Quentin 22.11.18
+// Flayac Quentin22/11/18
+// PHP Hand-Written Script to Transform GML SRS
 // --------------------------
 
-function SRSRewriter($argv){
-  if(isset($argv)){
+function main($argv) {
+  if(isset($argv)) {
     // recursive folder
-    if($argv[1] === "-r"){
-      // basefolder -> filename (//countable)
-      $folder = scandir($argv[2]);
-      // 587
-      for($i = 0; $i < count($folder); $i++) {
-        $extension = explode(".", $folder[$i]);
-        $extPlacement = count($extension);
-        if($extension[$extPlacement - 1] === "gml") {
-          // basefile content in an array, exploded with \n
-          $array = explode("\n", file_get_contents($argv[2]."/".$folder[$i]));
-          // basefile name
-          $filename = explode("/", $folder[$i]);
-          // argv 3 = dimension -- argv 4 === EPSG
-          for($i = 0; $i < count($array); $i++) {
-            $srsReplace = "srsDimension=\"".$argv[3]."\"";
-            $srsUpdate = "srsName=\"EPSG:".$argv[4]."\" srsDimension=\"".$argv[3]."\"";
-            $array[$i] = str_replace($srsReplace, $srsUpdate, $array[$i]);
-          }
-          // create a new File with modification
-          file_put_contents(__DIR__."/rewrited/rewrited_".$filename[count($filename) - 1], implode("\n", $array));
-        }
-      }
+    if($argv[1] === "-r") {
+      recursiveRewrite($argv);
     }
     else {
-      // basefile content in an array, exploded with \n
-      $array = explode("\n", file_get_contents($argv[1]));
-      // basefile name
-      $filename = explode("/", $argv[1]);
-      // argv 2 = dimension -- argv 3 === epsg
-      for($i = 0; $i < count($array); $i++) {
-        $srsReplace = "srsDimension=\"".$argv[2]."\"";
-        $srsUpdate = "srsName=\"EPSG:".$argv[3]."\" srsDimension=\"".$argv[2]."\"";
-        $array[$i] = str_replace($srsReplace, $srsUpdate, $array[$i]);
-      }
-      // create a new File with modification
-      file_put_contents(__DIR__."/rewrited/rewrited_".$filename[count($filename) - 1], implode("\n", $array));
+      baseRewrite($argv);
     }
   }
 }
-SRSRewriter($argv);
+function logger($str) {
+  echo $str . "\n";
+  return;
+}
+function recursiveRewrite($argv) {
+  logger("\nStarting Recursive Rewrite...");
+  // basefolder -> filename (//countable)
+  $folder = scandir($argv[2]);
+  // 587
+  for($i = 0; $i < count($folder); $i++) {
+    $extension = explode(".", $folder[$i]);
+    $extPlacement = count($extension);
+    if($extension[$extPlacement - 1] === "gml") {
+      // basefile content in an array, exploded with \n
+      $array = explode("\n", file_get_contents($argv[2]."/".$folder[$i]));
+      // basefile name
+      $filename = explode("/", $folder[$i]);
+      // argv 3 = dimension -- argv 4 === epsg
+      for($y = 0; $y < count($array); $y++) {
+        $srsReplace = "srsDimension=\"".$argv[3]."\"";
+        $srsUpdate = "srsName=\"EPSG:".$argv[4]."\" srsDimension=\"".$argv[3]."\"";
+        $array[$y] = str_replace($srsReplace, $srsUpdate, $array[$y]);
+      }
+      // create a new File with modification
+      logger("File saved at: " . __DIR__."/rewrited/rewrited_".$filename[count($filename) - 1]);
+      file_put_contents(__DIR__."/rewrited/rewrited_".$filename[count($filename) - 1], implode("\n", $array));
+    }
+  }
+  logger("Done.");
+  return;
+}
+function baseRewrite($argv) {
+  logger("\nStarting...");
+  // basefile content in an array, exploded with \n
+  $array = explode("\n", file_get_contents($argv[1]));
+  // basefile name
+  $filename = explode("/", $argv[1]);
+  // argv 2 = dimension -- argv 3 === epsg
+  for($i = 0; $i < count($array); $i++) {
+    $srsReplace = "srsDimension=\"".$argv[2]."\"";
+    $srsUpdate = "srsName=\"EPSG:".$argv[3]."\" srsDimension=\"".$argv[2]."\"";
+    $array[$i] = str_replace($srsReplace, $srsUpdate, $array[$i]);
+  }
+  // create a new File with modification
+  logger("File saved at: " . __DIR__."/rewrited/rewrited_".$filename[count($filename) - 1]);
+  file_put_contents(__DIR__."/rewrited/rewrited_".$filename[count($filename) - 1], implode("\n", $array));
+  logger("Done.");
+  return;
+}
+
+
+main($argv);
